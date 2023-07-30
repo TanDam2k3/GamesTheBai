0cb08d60a53f19372bfbf73eea37eb26671dbf23
tandam2k3 <tandam9902@gmail.com>
2023-07-30 10:18:16 +0700

    ver8

--- db/java/index.js
+++ db/java/index.js
@@ -95,17 +95,17 @@ function showResult(){
 
 
 function run(){
-    document.addEventListener("keydown", function (event){
-        if (event.ctrlKey){
-           event.preventDefault();
-        }
-        if(event.keyCode == 123){
-           event.preventDefault();
-        }
-    });
-    document.addEventListener('contextmenu',
-        event => event.preventDefault()
-    );
+    // document.addEventListener("keydown", function (event){
+    //     if (event.ctrlKey){
+    //        event.preventDefault();
+    //     }
+    //     if(event.keyCode == 123){
+    //        event.preventDefault();
+    //     }
+    // });
+    // document.addEventListener('contextmenu',
+    //     event => event.preventDefault()
+    // );
     setInterval(timeStart, 1000);
     setInterval(showResult,1000);
 }
--- db/java/start.js
+++ db/java/start.js
@@ -11,17 +11,17 @@ function timeEnd(time) {
 }
 
 function run(){
-    document.addEventListener("keydown", function (event){
-        if (event.ctrlKey){
-           event.preventDefault();
-        }
-        if(event.keyCode == 123){
-           event.preventDefault();
-        }
-    });
-    document.addEventListener('contextmenu',
-        event => event.preventDefault()
-    );
+    // document.addEventListener("keydown", function (event){
+    //     if (event.ctrlKey){
+    //        event.preventDefault();
+    //     }
+    //     if(event.keyCode == 123){
+    //        event.preventDefault();
+    //     }
+    // });
+    // document.addEventListener('contextmenu',
+    //     event => event.preventDefault()
+    // );
     console.log(localStorage.getItem('point'));
 }
 
